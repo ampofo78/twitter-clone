ddffcdbaa92d76916dea2729add8316c3bd7f329
Ampofo Oduro <Ampofoodurodaniel@gmail.com>
2026-03-24 08:39:43 +0000

    new change in backend

---
+++ backend/src/server.js
@@ -0,0 +1,52 @@
+import { clerkMiddleware } from "@clerk/express";
+import cors from "cors";
+import express from "express";
+import userRoutes from "../src/Routes/user.route.js";
+import { connectDb } from "./config/db.js";
+import { ENV } from "./config/env.js";
+import { arcjetMiddleware } from "./middleware/arcjet.middleware.js";
+import commentRoutes from "./Routes/comment.route.js";
+import notificationRoutes from "./Routes/notification.route.js";
+import postRoutes from "./Routes/post.route.js";
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
+app.use(clerkMiddleware());
+app.use(arcjetMiddleware);
+
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/notifications", notificationRoutes);
+app.use((err, req, res, next) => {
+  console.error("Unhandled error:", err);
+  res.status(500).json({ error: err.message || "Internal server error" });
+});
+
+app.get("/api/health", (req, res) => {
+  return res.status(200).json({ status: "ok" });
+});
+app.get("/", (req, res) => {
+  return res.send("Hello from server");
+});
+const startServer = async () => {
+  try {
+    await connectDb();
+
+   if (ENV.NODE_ENV !== 'production') {
+     app.listen(ENV.PORT);
+   }
+  } catch (error) {
+    console.log("there is error in starting server", error.message);
+    process.exit(1);
+  }
+};
+
+startServer();
+export default app;
